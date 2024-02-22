fdaaff8e284a3fbfee705a868dbe749dfe885879
Paulo Alexandre <paulofore@gmail.com>
2024-02-22 01:04:17 -0300

    semanal

--- workspace-eclipse2/geraangular/geraangular/src/main/java/br/com/gersis/geraangular/CriaArquivos.java
+++ workspace-eclipse2/geraangular/geraangular/src/main/java/br/com/gersis/geraangular/CriaArquivos.java
@@ -1,6 +1,7 @@
 package br.com.gersis.geraangular;
 
 import java.io.File;
+import java.io.IOException;
 
 import br.com.gersis.daobase.DaoBase;
 import br.com.gersis.daobase.DummyDaoBase;
@@ -28,15 +29,16 @@ public class CriaArquivos extends DaoAplicacao{
 		Sistema sistema = ds.getSistema();
 		String pathPadrao = "loopback-angular6/src/app";
 		//String pathArquivos = sistema.getPathProjeto() + File.separator + pathPadrao + File.separator;
-		this.verificaECriaSubdiretorios(sistema.getPathProjeto(),pathPadrao);
+		
 		String dirComponente = "";
 		try {
+			this.verificaECriaSubdiretorios(sistema.getPathProjeto(),pathPadrao);
 			for (Entidade entidade : sistema.getEntidades()) {
 				
 				for (TelaFront tela : entidade.getTelaFronts()) {
 					
 					dirComponente = tela.getNomeHifen();
-					this.verificaECriaSubdiretorios(sistema.getPathProjeto() + File.separator + pathPadrao,dirComponente);
+					this.verificaECriaSubdiretorios(sistema.getPathProjeto() + File.separator + pathPadrao,dirComponente, tela);
 					String diretorio = sistema.getPathProjeto() + File.separator + pathPadrao + File.separator + dirComponente;
 
 					GeraTs gerador1 = FabricaGerador.criaTs(tela, diretorio);
@@ -72,14 +74,29 @@ public class CriaArquivos extends DaoAplicacao{
 		finalizar();
 	}
 	
-	private void verificaECriaSubdiretorios(String principal, String caminho) {
-		 File diretorio = new File(principal);
+	private void verificaECriaSubdiretorios(String principal, String caminho, TelaFront tela) throws InterruptedException, IOException {
+	   File diretorio = new File(principal);
        // Cria os subdiretórios
        String[] subdiretorios = caminho.split("/");
+       String rootProjeto =  principal.replace("/src/app", "");
        for (String subdiretorio : subdiretorios) {
            diretorio = new File(diretorio, subdiretorio);
            if (!diretorio.exists()) {
+        	   String comando;
                if (diretorio.mkdir()) {
+            	   comando = "cd " + rootProjeto;
+                   ProcessBuilder pb1 = new ProcessBuilder().command("bash", "-c", comando);
+                   pb1.start().waitFor();
+
+                   // Segundo comando
+                   comando = "ng g component " + tela.getNome();
+                   ProcessBuilder pb2 = new ProcessBuilder().command("bash", "-c", comando);
+                   pb2.start().waitFor();
+
+                   // Terceiro comando
+                   comando = "rm -f src/app/" + tela.getNomeHifen() + "/*";
+                   ProcessBuilder pb3 = new ProcessBuilder().command("bash", "-c", comando);
+                   pb3.start().waitFor();
             	   //System.out.println("Subdiretório '" + subdiretorio + "' criado.");
                } else {
             	   //System.out.println("Subdiretório '" + subdiretorio + "' não criado.");
@@ -90,6 +107,24 @@ public class CriaArquivos extends DaoAplicacao{
        }
 	}
 
+	private void verificaECriaSubdiretorios(String principal, String caminho) throws InterruptedException, IOException {
+		 File diretorio = new File(principal);
+      // Cria os subdiretórios
+      String[] subdiretorios = caminho.split("/");
+      for (String subdiretorio : subdiretorios) {
+          diretorio = new File(diretorio, subdiretorio);
+          if (!diretorio.exists()) {
+              if (diretorio.mkdir()) {
+                 
+           	   //System.out.println("Subdiretório '" + subdiretorio + "' criado.");
+              } else {
+           	   //System.out.println("Subdiretório '" + subdiretorio + "' não criado.");
+              }
+          } else {
+              //System.out.println("Subdiretório '" + subdiretorio + "' já existe.");
+          }
+      }
+	}
 	
 	@Override
 	protected DaoBase getProximo() {
